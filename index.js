7318e6e8e4180d8be0f90e16f2fa4819a00e6b90
Jackie <jackie.yuen656@gmail.com>
2024-12-11 16:04:29 -0500

    Noob branch

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-
+//Noob noob
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
