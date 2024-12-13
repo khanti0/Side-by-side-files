5f11e95e643923799f7acdb8908cf59db2658210
Jackie <jackie.yuen656@gmail.com>
2024-12-12 22:45:14 -0500

    Finished product

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const multer = require('multer')
 const path = require('path')
-const fs = require('fs')
+const fs1 = require('fs')
+const fs = require('fs').promises
 const app = express()
-
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './uploads')
@@ -12,59 +12,53 @@ const fileStorageEngine = multer.diskStorage({
     cb(null, file.originalname)
   }
 })
-
 const upload = multer({ storage: fileStorageEngine })
-
-let files = [
-  {
-    id: 1,
-    content: "HTML is easier",
-    important: true
-  }
-]
-
 app.use(express.static('dist'))
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
 const cors = require('cors')
-
 app.use(cors())
-
 app.use(express.json())
-app.use(requestLogger)
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.get('/api/files', (request, response) => {
-  response.json(files)
-})
+const libre = require('libreoffice-convert');
+libre.convertAsync = require('util').promisify(libre.convert);
 
 app.post('/api/files', upload.single('file'), (request, response) => {
-  
-  response.json(request.file)
-
-  fs.readdir(directory, (err, files) => {
+  console.log('noob')
+  fs1.readdir('./uploads', (err, files) => {
     if (err) throw err;
-  
+    console.log('noob')
     for (const file of files) {
-      fs.unlink(path.join('./uploads', file), (err) => {
+      fs1.unlink(path.join('./uploads', file), (err) => {
         if (err) throw err;
-      });
+      })
     }
-  });
+  })
+
+  if(request.file.mimetype === 'application/vnd.openxmlformats-officedocument.presentationml.presentation'){
+    
+    async function main() {
+      const ext = '.pdf'
+      const fileNameLength = request.file.originalname.length - 5
+      const newFileName = request.file.originalname.substring(0, fileNameLength)
+      const inputPath = path.join('./uploads', request.file.originalname);
+      const outputPath = path.join('./uploads', `${newFileName}.pdf`);
+      // Read file
+      const docxBuf = await fs.readFile(inputPath);
+      // Convert it to pdf format with undefined filter (see Libreoffice docs about filter)
+      let pdfBuf = await libre.convertAsync(docxBuf, ext, undefined);
+      // Here in done you have pdf file which you can save or transfer in another stream
+      await fs.writeFile(outputPath, pdfBuf);
+      await response.sendFile(`${newFileName}.pdf`, { root: 'uploads' });
+    }
+    
+    main().catch(function (err) {
+      console.log(`Error converting file: ${err}`);
+    });
+
+  } else {
+     
+  }
 
 })
-
-app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
